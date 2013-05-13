85e8516415c0cab05aa922820198cdb9d6f00908
Tehed <chrstfranke@gmx.net>
2013-05-14 00:16:17 +0200

    added scene; added projectile

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,12 +4,17 @@ using System.Collections;
 public class Player : MonoBehaviour {
 	
 	private Transform myTransform;
+	private int screenWidth; 
+	private int screenHeight;
 	
 	public int playerSpeed = 5;
 	
 	// Use this for initialization
 	void Start () {
 		
+		screenWidth = Screen.width;
+		screenHeight = Screen.height;
+		Debug.Log("Screen: " + screenWidth + "x" + screenHeight, this);
 		myTransform = transform;
 		
 		// Spawn point
@@ -22,6 +27,15 @@ public class Player : MonoBehaviour {
 	
 		// move the player left and right
 		myTransform.Translate(Vector3.right * playerSpeed * Time.deltaTime * Input.GetAxis("Horizontal"));
+	
+		// player wrap at the edges
+		if(myTransform.position.x < -17) {
+			myTransform.position = new Vector3(17, myTransform.position.y, myTransform.position.z);
+		} else if (myTransform.position.x > 17) {
+			myTransform.position = new Vector3(-17, myTransform.position.y, myTransform.position.z);
+		}
+		
+		// press space bar to fire a laser
 		
 	}
 }
